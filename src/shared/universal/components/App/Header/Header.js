ea49960a7414bde55cbb5b2fb5b9f8222376c5cd
Attila Szeremi <attila@szeremi.org>
2016-12-07 19:51:51 +0100

    Remove logo and and other unneeded stuff

--- src/shared/universal/components/App/Header/Header.js
+++ src/shared/universal/components/App/Header/Header.js
@@ -1,16 +1,13 @@
 /* @flow */
 
 import React from 'react';
-import Logo from './Logo';
 import Menu from './Menu';
-import { WEBSITE_DESCRIPTION } from '../../../constants';
+import { WEBSITE_TITLE } from '../../../constants';
 
 function Header() {
   return (
     <div style={{ textAlign: 'center', marginBottom: '1rem' }}>
-      <Logo />
-      <h1>React, Universally</h1>
-      <strong>{WEBSITE_DESCRIPTION}</strong>
+      <h1>{ WEBSITE_TITLE }</h1>
 
       <Menu />
     </div>
--- src/shared/universal/constants.js
+++ src/shared/universal/constants.js
@@ -1,8 +1,4 @@
 /* @flow */
 /* eslint-disable import/prefer-default-export */
 
-export const WEBSITE_TITLE = 'React Universally';
-
-export const WEBSITE_DESCRIPTION =
-  'A starter kit giving you the minimum requirements for a production ready ' +
-  'universal react application.';
+export const WEBSITE_TITLE = 'Teamap - coordinator';
